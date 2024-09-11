4700e44a1d76c57b2f6c2897591b3a7b11724e32
subasish bag <subasishbag77@gmail.com>
2024-09-11 09:53:40 +0530

    the file is for demonstration purpose

---
+++ arguments.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+    return a + b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function doOperation(a, b, op) {
+    return op(a, b)
+  }
+  
+  console.log(sum(doOperation(5, 2, multiply), doOperation(5, 2, divide)))
